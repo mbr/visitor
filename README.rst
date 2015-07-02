ffeb83f9856191377f228dc1031e11e9824c5310
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-02 15:47:00 +0200

    Initial import.

---
+++ README.rst
@@ -0,0 +1,7 @@
+visitor
+=======
+
+A tiny library to facilitate `visitor
+<https://en.wikipedia.org/wiki/Visitor_pattern>`_ implementation in Python
+(which are slightly peculiar due to dynamic typing).
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='visitor',
+    version='0.1.dev1',
+    description='A tiny visitor implementation for Python.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/visitor',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
+)
